c08ede612000648451f9b137be604d3ee099ad2f
József Sallai <jozsef@sallai.me>
2021-07-21 02:37:02 +0300

    make guild list item design more consistent

--- components/guilds/GuildListItem.tsx
+++ components/guilds/GuildListItem.tsx
@@ -41,20 +41,22 @@ const GuildListItem = ({ guild, onClick }: GuildListItemProps) => {
     : undefined;
 
   return (
-    <div className="flex items-center gap-4 py-3 my-2">
-      <GuildIcon guild={guild} onClick={handleGuildClick} />
+    <div className="lg:flex items-center justify-between gap-3 my-4">
+      <div className="flex items-center gap-3">
+        <GuildIcon guild={guild} onClick={handleGuildClick} />
 
-      <div>
-        <div className={titleClassNames} onClick={handleGuildClick}>{guild.name}</div>
-
-        {'key' in guild && guild.key && (
-          <div className="lg:flex items-center gap-3 text-sm">
-            <div>{guild.key}</div>
-            <Button size={ButtonSize.SMALL} onClick={handleCopyClick} className="mr-3 lg:mr-0">{copyButtonText}</Button>
-            <Button size={ButtonSize.SMALL} onClick={handleRegenerateClick}>Regenerate</Button>
-          </div>
-        )}
+        <div className="flex-1">
+          <div className={titleClassNames} onClick={handleGuildClick}>{guild.name}</div>
+          {'key' in guild && guild.key && <span className="text-xs">{guild.key}</span>}
+        </div>
       </div>
+
+      {'key' in guild && guild.key && (
+        <div className="flex gap-2 my-4 lg:my-0">
+          <Button size={ButtonSize.SMALL} onClick={handleCopyClick} className="mr-3 lg:mr-0">{copyButtonText}</Button>
+          <Button size={ButtonSize.SMALL} onClick={handleRegenerateClick}>Regenerate</Button>
+        </div>
+      )}
     </div>
   );
 };
