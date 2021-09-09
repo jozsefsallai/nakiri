9d636fd58de69f09c992b8dd193cce1bc31cce36
József Sallai <jozsef@sallai.me>
2021-09-10 02:55:41 +0300

    whoops

--- pages/api/monitored-keywords/guild/[guild]/whitelisted-channels/[id].ts
+++ pages/api/monitored-keywords/guild/[guild]/whitelisted-channels/[id].ts
@@ -3,7 +3,7 @@ import { withSentry } from '@sentry/nextjs';
 
 import { ensureAuthenticated } from '@/middleware/auth';
 
-import * as keywordWhitelistedChannelsController from '@/controllers/keyword-base-invertlisted-channels/keywordWhitelistedChannelsController';
+import * as keywordWhitelistedChannelsController from '@/controllers/keyword-whitelisted-channels/keywordWhitelistedChannelsController';
 
 export default bar({
   delete: withSentry(ensureAuthenticated(keywordWhitelistedChannelsController.destroy, true)),
